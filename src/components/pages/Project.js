f445ca5f32b6ea6410056c4225f2a1823d820888
Georgia <ghobs987@gmail.com>
2021-12-16 03:19:02 +0000

    imgs for portfolio page and working portfolio pg

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,5 +1,12 @@
 import React from "react";
 import techBlogImg from "../../img/screenshottechblog.png";
+import FiveStarLocal from "../../img/5starlocal.png";
+import EmployeeManager from "../../img/employeemanager.png"
+import Inquistive from "../../img/inquisitive.png"
+import Pwordgen from "../../img/passwordgenerator.PNG"
+import Weatherdash from "../../img/weatherdashboard.png"
+
+
 
 export default function Projects() {
   return (
@@ -12,65 +19,64 @@ export default function Projects() {
       </p>
 
       <div className="projectbox">
-        <h2>a project</h2>
+        <h2>Tech Blog</h2>
         <h3>please click on the image to be taken to the deployed version</h3>
         <a href="https://dry-eyrie-85153.herokuapp.com/">
           <img src={techBlogImg} className="imgstyling" alt="techblog img" />
         </a>
-        <p>a description</p>
+        <p>A difficult and time consuming project, with both a working front and backend and login logic using, sequlizes, handlebars, cookies and api calls</p>
         <a className="repolnk"href="https://github.com/G-bot987/TechBlog"> the repo</a>
       </div>
       
       <div className="projectbox">
-        <h2>a project</h2>
+        <h2>5StarLocal Group Project</h2>
         <h3>please click on the image to be taken to the deployed version</h3>
-        <a href="https://dry-eyrie-85153.herokuapp.com/">
-          <img src={techBlogImg} className="imgstyling" alt="techblog img" />
+        <a href="https://vast-journey-10843.herokuapp.com/login">
+          <img src={FiveStarLocal} className="imgstyling" alt="techblog img" />
         </a>
-        <p>a description</p>
-        <a className="repolnk"href="https://github.com/G-bot987/TechBlog"> the repo</a>
+        <p>A group project with  similar functionality to TechBlog </p>
+        <a className="repolnk"href="https://github.com/G-bot987/My-5-Star-Local"> the repo</a>
       </div>
 
       <div className="projectbox">
-        <h2>a project</h2>
+        <h2>Employee Manager</h2>
         <h3>please click on the image to be taken to the deployed version</h3>
-        <a href="https://dry-eyrie-85153.herokuapp.com/">
-          <img src={techBlogImg} className="imgstyling" alt="techblog img" />
+        <a href=" https://drive.google.com/file/d/16ZVj7zfFe_IYRHH2Utjb4Rl4dU076VWi/view">
+          <img src={EmployeeManager} className="imgstyling" alt="techblog img" />
         </a>
-        <p>a description</p>
-        <a className="repolnk"href="https://github.com/G-bot987/TechBlog"> the repo</a>
+        <p>A backend commandline application with employee management application</p>
+        <a className="repolnk"href="https://github.com/G-bot987/employee-manager-29.08.21"> the repo</a>
       </div>
 
       <div className="projectbox">
-        <h2>a project</h2>
+        <h2>Inquistive Group Project</h2>
         <h3>please click on the image to be taken to the deployed version</h3>
-        <a href="https://dry-eyrie-85153.herokuapp.com/">
-          <img src={techBlogImg} className="imgstyling" alt="techblog img" />
+        <a href="https://uk-inquizitive.herokuapp.com/">
+          <img src={Inquistive} className="imgstyling" alt="techblog img" />
         </a>
-        <p>a description</p>
-        <a className="repolnk"href="https://github.com/G-bot987/TechBlog"> the repo</a>
+        <p>The most complicated application that I have worked on, A full stack quiz application</p>
+        <a className="repolnk"href="https://github.com/G-bot987/inQuizitive"> the repo</a>
       </div>
 
       <div className="projectbox">
-        <h2>a project</h2>
+        <h2>Password Generator</h2>
         <h3>please click on the image to be taken to the deployed version</h3>
-        <a href="https://dry-eyrie-85153.herokuapp.com/">
-          <img src={techBlogImg} className="imgstyling" alt="techblog img" />
+        <a href="https://g-bot987.github.io/20.06.21/">
+          <img src={Pwordgen} className="imgstyling" alt="techblog img" />
         </a>
-        <p>a description</p>
-        <a className="repolnk"href="https://github.com/G-bot987/TechBlog"> the repo</a>
-      </div>
-
-      <div className="projectbox">
-        <h2>a project</h2>
-        <h3>please click on the image to be taken to the deployed version</h3>
-        <a href="https://dry-eyrie-85153.herokuapp.com/">
-          <img src={techBlogImg} className="imgstyling" alt="techblog img" />
-        </a>
-        <p>a description</p>
-        <a className="repolnk"href="https://github.com/G-bot987/TechBlog"> the repo</a>
+        <p>A javascript password generator, that works to user criteria</p>
+        <a className="repolnk"href="https://github.com/G-bot987/20.06.21"> the repo</a>
       </div>
       
+      <div className="projectbox">
+        <h2>Weatherdashboard</h2>
+        <h3>please click on the image to be taken to the deployed version</h3>
+        <a href="https://g-bot987.github.io/weatherdashboard/">
+          <img src={Weatherdash} className="imgstyling" alt="techblog img" />
+        </a>
+        <p>a description</p>
+        <a className="repolnk"href="https://github.com/G-bot987/weatherdashboard"> the repo</a>
+      </div>
 
 
 
