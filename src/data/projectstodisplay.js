3a69c7857a67837f72ac371d812063d50cd232c9
Georgia <ghobs987@gmail.com>
2022-10-29 13:21:37 +0100

    new data for projects

--- src/data/projectstodisplay.js
+++ src/data/projectstodisplay.js
@@ -1,9 +1,62 @@
 const projects = [
   {
-    header: "string",
-    description: "string",
-    links: { deployed: "string", repo: "string" },
-    img: { ref: "string", alt: "string" },
+    header: "Tech Blog",
+    description:
+      "A difficult and time consuming project, with both a working front and backend and login logic using, sequlizes, handlebars, cookies and api calls",
+    links: {
+      deployed: "https://dry-eyrie-85153.herokuapp.com/",
+      repo: "https://github.com/G-bot987/TechBlog",
+    },
+    img: { ref: "techBlogImg", alt: "techblog img" },
+  },
+  {
+    header: "5StarLocal Group Project",
+    description: "A group project with  similar functionality to TechBlog",
+    links: {
+      deployed: "https://vast-journey-10843.herokuapp.com/login",
+      repo: "https://github.com/G-bot987/My-5-Star-Local",
+    },
+    img: { ref: "FiveStarLocal", alt: "5 star local img" },
+  },
+  {
+    header: "Employee Manager",
+    description:
+      "A backend commandline application with employee management application",
+    links: {
+      deployed:
+        "https://drive.google.com/file/d/16ZVj7zfFe_IYRHH2Utjb4Rl4dU076VWi/view",
+      repo: "https://github.com/G-bot987/employee-manager-29.08.21",
+    },
+    img: { ref: "EmployeeManager", alt: "employee manager img" },
+  },
+  {
+    header: "Inquistive Group Project",
+    description:
+      "The most complicated application that I have worked on, A full stack quiz application",
+    links: {
+      deployed: "https://uk-inquizitive.herokuapp.com/",
+      repo: "https://github.com/G-bot987/inQuizitive",
+    },
+    img: { ref: "Inquistive", alt: "Inquistive img" },
+  },
+  {
+    header: "Password Generator",
+    description: "A javascript password generator, that works to user criteria",
+    links: {
+      deployed: "https://g-bot987.github.io/20.06.21/",
+      repo: "https://github.com/G-bot987/20.06.21",
+    },
+    img: { ref: "Pwordgen", alt: "Pword Generator IMG" },
+  },
+  {
+    header: "Weatherdashboard",
+    description:
+      "Shows you weather conditions for a selected location, using api calls",
+    links: {
+      deployed: "https://g-bot987.github.io/weatherdashboard/",
+      repo: "https://github.com/G-bot987/weatherdashboard",
+    },
+    img: { ref: "Weatherdash", alt: "weather dashboard img" },
   },
 ];
 
