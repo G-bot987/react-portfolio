c8815a153094c84c680e2ec1023f972c4da95c3f
Georgia <ghobs987@gmail.com>
2022-10-29 13:21:15 +0100

    link updates for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 import Octocat from "../img/Octocat.png";
 import lnkin from "../img/LI-In-Bug.png";
 export default function Footer() {
   return (
     <div>
-        <p>
+      <p>
+        <a href="https://github.com/G-bot987">
+          <img src={Octocat} className="octocat" alt="Octocat img" />
+        </a>
 
-<a href="https://github.com/G-bot987"> 
-<img src={Octocat} className="octocat" alt="Octocat img" />
-</a>
-
-<a href="https://www.linkedin.com/in/georgia-hobson-b48434150/"> 
-<img src={lnkin} className="lnkin" alt="my linked in img" />
-</a>
+        <a href="https://www.linkedin.com/in/georgia-h-b48434150/">
+          <img src={lnkin} className="lnkin" alt="my linked in img" />
+        </a>
       </p>
     </div>
   );
